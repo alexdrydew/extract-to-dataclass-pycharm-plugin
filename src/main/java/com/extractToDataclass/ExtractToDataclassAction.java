f50da8824e5e4635febb45f6c33edd43f57d66f5
Aleksey Sukhorosov <alexdrydew@toloka.ai>
2024-06-28 23:51:05 +0200

    impove extraction

--- src/main/java/com/extractToDataclass/ExtractToDataclassAction.java
+++ src/main/java/com/extractToDataclass/ExtractToDataclassAction.java
@@ -1,12 +1,14 @@
 package com.extractToDataclass;
 
+import com.google.common.base.CaseFormat;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
 import com.jetbrains.python.psi.*;
+import com.jetbrains.python.psi.impl.PyPsiUtils;
+import com.jetbrains.python.psi.resolve.RatedResolveResult;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
@@ -14,10 +16,11 @@ import java.util.List;
 import java.util.Vector;
 
 public class ExtractToDataclassAction extends AnAction {
+    private static final String DATACLASS_IDENTIFIER = "dataclass";
+
     @Override
     public void actionPerformed(AnActionEvent event) {
         PyFunction function = getTargetFunction(event);
-
 
         if (function != null) {
             PyParameterList params = function.getParameterList();
@@ -25,7 +28,10 @@ public class ExtractToDataclassAction extends AnAction {
                 return;
             }
 
-            ExtractToDataclassDialogWrapper dialog = new ExtractToDataclassDialogWrapper(Arrays.stream(function.getParameterList().getParameters()).map(PyParameter::getName).toArray(String[]::new));
+            ExtractToDataclassDialogWrapper dialog = new ExtractToDataclassDialogWrapper(
+                    Arrays.stream(function.getParameterList().getParameters())
+                            .map(PyParameter::getName)
+                            .toArray(String[]::new));
             boolean isOk = dialog.showAndGet();
             if (!isOk) {
                 return;
@@ -35,39 +41,64 @@ public class ExtractToDataclassAction extends AnAction {
                 throw new IllegalStateException("parametersIndicesToExtract is null");
             }
 
-            PsiFile targetFile = function.getContainingFile();
-            String paramsDataclassStringBuilder = buildParamsDataclassSource(params.getParameters(), parametersIndicesToExtract);
+            String dataclassName = "%sParams".formatted(
+                    CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, function.getName()));
+            String dataclassSource = buildParamsDataclassSource(params.getParameters(), dataclassName, parametersIndicesToExtract);
             PyClass clazz = PyElementGenerator.getInstance(function.getProject()).createFromText(
-                    LanguageLevel.getDefault(), PyClass.class, paramsDataclassStringBuilder.toString()
+                    LanguageLevel.getDefault(), PyClass.class, dataclassSource.toString()
             );
-            PyFromImportStatement importDataclass = PyElementGenerator.getInstance(function.getProject()).createFromText(
-                    LanguageLevel.getDefault(),
-                    PyFromImportStatement.class,
-                    "from dataclasses import dataclass\n"
-            );
-            WriteCommandAction.runWriteCommandAction(
-                    function.getProject(),
-                    "Add dataclass import",
-                    null,
-                    () -> targetFile.addBefore(importDataclass, targetFile.getFirstChild())
-            );
+
+            PyFile targetFile = (PyFile) function.getContainingFile();
+
+            if (!isDataclassImported(targetFile)) {
+                PyFromImportStatement importDataclass = PyElementGenerator.getInstance(function.getProject()).createFromImportStatement(
+                        LanguageLevel.getDefault(),
+                        "dataclasses",
+                        DATACLASS_IDENTIFIER,
+                        null);
+                WriteCommandAction.runWriteCommandAction(
+                        function.getProject(),
+                        "Add dataclass import",
+                        null,
+                        () -> targetFile.addBefore(importDataclass, targetFile.getFirstChild()));
+            }
             WriteCommandAction.runWriteCommandAction(
                     function.getProject(),
                     "Create class",
                     null,
-                    () -> targetFile.addBefore(clazz, function)
+                    () -> targetFile.addBefore(clazz, function));
+            WriteCommandAction.runWriteCommandAction(
+                    function.getProject(),
+                    "Create parameter",
+                    null,
+                    () -> params.addParameter(PyElementGenerator.getInstance(function.getProject()).createParameter(
+                            "params", null, dataclassName, LanguageLevel.getDefault()
+                    ))
             );
             removeParameters(function, parametersIndicesToExtract);
         }
     }
 
+    private static boolean isDataclassImported(PyFile targetFile) {
+        for (PyFromImportStatement fromImport : targetFile.getFromImports()) {
+            if (!fromImport.getImportSource().asQualifiedName().toString().equals("dataclasses")) continue;
+            for (PyImportElement importElement : fromImport.getImportElements()) {
+                if (importElement.getVisibleName().equals(DATACLASS_IDENTIFIER)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     private static @NotNull String buildParamsDataclassSource(
             PyParameter[] params,
+            String dataclassName,
             List<Integer> parametersIndicesToExtract
     ) {
         StringBuilder paramsDataclassStringBuilder = new StringBuilder();
         paramsDataclassStringBuilder.append("@dataclass\n");
-        paramsDataclassStringBuilder.append("class Params:\n");
+        paramsDataclassStringBuilder.append("class %s:\n".formatted(dataclassName));
         for (Integer index : parametersIndicesToExtract) {
             PyNamedParameter namedParam = params[index].getAsNamed();
             paramsDataclassStringBuilder.append("    ");
